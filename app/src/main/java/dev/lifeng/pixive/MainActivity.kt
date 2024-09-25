b3347071803ab2975f4ca0ef67a6754b96764df5
Li Feng <maplestarplay@gmail.com>
2024-09-25 23:03:05 +0800

    beautify style: make navigation bar transparent and modify outfit of menu

--- app/src/main/java/dev/lifeng/pixive/MainActivity.kt
+++ app/src/main/java/dev/lifeng/pixive/MainActivity.kt
@@ -1,44 +1,50 @@
 package dev.lifeng.pixive
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
-import dev.lifeng.pixive.infra.work.PeriodGetSpotlightsWork
-import dev.lifeng.pixive.infra.work.RefreshTokenWork
-import dev.lifeng.pixive.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         PixiveApplication.context = applicationContext
-        enableEdgeToEdge()
+        //TODO weird workaround
+        enableEdgeToEdge(navigationBarStyle = SystemBarStyle.light(1,10))
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.statusBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        WorkManager.getInstance(this).cancelAllWork()
         //set homeFragment as default fragment
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+        //supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+        //findNavController().navigate()
+
         //add refreshToken work to WorkManager
-        val refreshTokenWorkRequest = PeriodicWorkRequestBuilder<RefreshTokenWork>(10, java.util.concurrent.TimeUnit.MINUTES)
-                                        .setInitialDelay(10,java.util.concurrent.TimeUnit.MINUTES).build()
-        PixiveApplication.REFRESH_TOKEN_WORK_ID = refreshTokenWorkRequest.id
-        val refreshSpotlightsWorkRequest = PeriodicWorkRequestBuilder<PeriodGetSpotlightsWork>(1, java.util.concurrent.TimeUnit.DAYS)
-                                            .build()
-        WorkManager.getInstance(this).enqueue(refreshTokenWorkRequest)
-        WorkManager.getInstance(this).enqueue(refreshSpotlightsWorkRequest)
+        //val refreshTokenWorkRequest = PeriodicWorkRequestBuilder<RefreshTokenWork>(10, java.util.concurrent.TimeUnit.MINUTES)
+        //                                .setInitialDelay(10,java.util.concurrent.TimeUnit.MINUTES).build()
+        //PixiveApplication.REFRESH_TOKEN_WORK_ID = refreshTokenWorkRequest.id
+//        val refreshSpotlightsWorkRequest = PeriodicWorkRequestBuilder<PeriodGetSpotlightsWork>(1, java.util.concurrent.TimeUnit.DAYS)
+//            .setInitialDelay(10, java.util.concurrent.TimeUnit.SECONDS).build()
+//
+//        //WorkManager.getInstance(this).enqueue(refreshTokenWorkRequest)
+//        WorkManager.getInstance(this).enqueue(refreshSpotlightsWorkRequest)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         //cancel all work when activity destroy(Actually, it's not how workManager should be used...)
-        WorkManager.getInstance(this).cancelWorkById(PixiveApplication.REFRESH_TOKEN_WORK_ID)
+        //WorkManager.getInstance(this).cancelWorkById(PixiveApplication.REFRESH_TOKEN_WORK_ID)
+        //WorkManager.getInstance(this).cancelAllWork()
         Log.d("Work", "cancel refreshTokenWork")
     }
 }
